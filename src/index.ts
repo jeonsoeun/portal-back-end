55eb395f425c98d34951ff912d98469465096c49
Soeun Jun <jse525@neowiz.com>
2020-01-14 16:37:06 +0900

    기초 설정

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 const Koa = require('koa');
 
+const PORT = 4000;
+
 const app = new Koa();
 app.use((ctx) => {
   ctx.body = 'Hello World!';
 });
 
-app.listen(4000, () => {
-  console.log('server is listening to port 4000');
+app.listen(PORT, () => {
+  console.log(`server is listening to port ${PORT}`);
 });
