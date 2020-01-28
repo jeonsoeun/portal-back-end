62684a9f759a7276965a20f04f4b2348403c4791
Soeun Jun <jse525@neowiz.com>
2020-01-28 18:23:54 +0900

    시간정보가 저장이 안되서 수정

--- src/controller/members.ts
+++ src/controller/members.ts
@@ -5,7 +5,8 @@ import { connectDB } from './connection';
 export const inputNewMember = (id: string, name: string) => {
   const db = connectDB();
   const Member = mongoose.model('Member', memberSchema);
-  const newMember = new Member({ id: id, name: name, date: new Date() });
+  const newMember = new Member({ id: id, name: name, joinDate: new Date() });
+  // 멤버 찾아서 검사하는 로직 추가해야됨.
   newMember.save((err, newMember) => {
     if (err) return console.error(err);
     console.log('input new member success');
