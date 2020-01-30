d0859ec2819e9247d1461c7a82efc30a59234063
Soeun Jun <jse525@neowiz.com>
2020-01-30 11:19:32 +0900

    회원가입시 id검사 로직 추가

--- src/api/authenticate/methods/post.ts
+++ src/api/authenticate/methods/post.ts
@@ -8,9 +8,9 @@ router.post('register', '/register/:env/:id/:name', register);
 router.post('login', '/login/:env/:id', login);
 
 async function register(ctx: Koa.Context): Promise<void> {
-  const member = inputNewMember(ctx.params.id, ctx.params.name);
-  ctx.status = 200;
-  ctx.body = member;
+  const member = await inputNewMember(ctx.params.id, ctx.params.name);
+  ctx.status = member.status;
+  ctx.body = member.message;
 }
 
 async function login(ctx: Koa.Context): Promise<void> {}
--- src/controller/members.ts
+++ src/controller/members.ts
@@ -2,14 +2,32 @@ import * as mongoose from 'mongoose';
 import { memberSchema } from '../models/members';
 import { connectDB } from './connection';
 
-export const inputNewMember = (id: string, name: string) => {
+export const inputNewMember = async (id: string, name: string) => {
   const db = connectDB();
   const Member = mongoose.model('Member', memberSchema);
   const newMember = new Member({ id: id, name: name, joinDate: new Date() });
-  // 멤버 찾아서 검사하는 로직 추가해야됨.
-  newMember.save((err, newMember) => {
-    if (err) return console.error(err);
-    console.log('input new member success');
-  });
-  return newMember;
+  const result = { status: 200, success: false, member: {}, message: '', err: {} };
+  await Member.find({ id: id })
+    .then((member) => {
+      if (member.length) {
+        result.member = member;
+        result.success = false;
+        result.message = 'ID already exists.';
+      } else {
+        newMember.save((err, newMember) => {
+          if (err) {
+            result.err = err;
+            return;
+          }
+          result.member = newMember;
+          result.success = true;
+          result.message = 'Register successly';
+        });
+      }
+    })
+    .catch((err) => {
+      result.status = 500;
+      result.err = err;
+    });
+  return result;
 };
